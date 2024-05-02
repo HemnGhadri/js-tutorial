b075f55768b46b8f2cf75ec794624c15490cc9ec
hemn <hemn.ghadri@gmail.com>
2024-05-03 00:18:19 +0330

    add first and second tutorila

---
+++ file-2/javascript.js
@@ -0,0 +1,9 @@
+let word = "hello world"
+alert(word)
+word = word.replace("hello", "Hello")
+
+document.write(word)
+
+console.log(typeof word)
+console.log(20 == "20") //check just value
+console.log(20 === "20") //check value with type
